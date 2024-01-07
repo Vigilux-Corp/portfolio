c0dd517ebee58b89e733b1cc5f4f9a78d642b380
Vincent Otieno <vinnyotieno004@gmail.com>
2024-01-08 00:15:17 +0300

    changes

---
+++ src/constants/index.js
@@ -0,0 +1,113 @@
+import {  facebook, instagram, linkedin, twitter,   } from "../assets";
+
+export const navLinks = [
+  {
+    id: "home",
+    title: "Home",
+  },
+  {
+    id: "about us",
+    title: "About Us",
+  },
+  {
+    id: "services",
+    title: "Services",
+  },
+  {
+    id: "contact us",
+    title: "Contant Us",
+  },
+];
+
+
+
+
+export const footerLinks = [
+  {
+    title: "Useful Links",
+    links: [
+      {
+        name: "Content",
+        link: "https://www.vigilux.com/content/",
+      },
+      {
+        name: "Meet Our Team",
+        link: "https://www.vigilux.com/Meet-Our-Team/",
+      },
+      {
+        name: "Create",
+        link: "https://www.vigilux.com/create/",
+      },
+      {
+        name: "Explore",
+        link: "https://www.vigilux.com/explore/",
+      },
+      {
+        name: "Terms & Services",
+        link: "https://www.vigilux.com/terms-and-services/",
+      },
+    ],
+  },
+  {
+    title: "Community",
+    links: [
+      {
+        name: "Help Center",
+        link: "https://www.vigilux.com/help-center/",
+      },
+      {
+        name: "Partners",
+        link: "https://www.vigilux.com/partners/",
+      },
+      {
+        name: "Suggestions",
+        link: "https://www.vigilux.com/suggestions/",
+      },
+      {
+        name: "Blog",
+        link: "https://www.vigilux.com/blog/",
+      },
+      {
+        name: "Newsletters",
+        link: "https://www.vigilux.com/newsletters/",
+      },
+    ],
+  },
+  {
+    title: "Partner",
+    links: [
+      {
+        name: "Our Partner",
+        link: "https://www.vigilux.com/our-partner/",
+      },
+      {
+        name: "Become a Partner",
+        link: "https://www.vigilux.com/become-a-partner/",
+      },
+    ],
+  },
+];
+
+export const socialMedia = [
+  {
+    id: "social-media-1",
+    icon: instagram,
+    link: "https://www.instagram.com/",
+  },
+  {
+    id: "social-media-2",
+    icon: facebook,
+    link: "https://www.facebook.com/",
+  },
+  {
+    id: "social-media-3",
+    icon: twitter,
+    link: "https://www.twitter.com/",
+  },
+  {
+    id: "social-media-4",
+    icon: linkedin,
+    link: "https://www.linkedin.com/",
+  },
+];
+
